4043ec3c9e0d64abe895517ce2596d7c3ea8a2ce
jheshengyang <71785711+jheshengyang@users.noreply.github.com>
2020-11-25 11:48:47 -0500

    Lab2 update

--- powershell/welcome.ps1
+++ powershell/welcome.ps1
@@ -17,7 +17,7 @@ function welcome {
 function get-mydisks {
     get-wmiobject win32_diskdrive | foreach {
         new-object -typename psobject -property @{Manufacturer=$_.Manufacturer;Model=$_.Model;
-            SerialNumber=$_.SerialNumber;FirmwareRevision=$_.FirmwareVersion;Size=($_.size / 1GB).tostring() + " GB"}
+            SerialNumber=$_.SerialNumber;FirmwareRevision=$_.FirmwareRevision;Size=($_.size / 1GB).tostring() + " GB"}
     }
 } 
 #----------------------------------------------------------------------------------
