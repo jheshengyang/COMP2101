6a3f223c508f229111fff0bf7c3641b9a70a2531
jheshengyang <71785711+jheshengyang@users.noreply.github.com>
2020-11-25 11:26:26 -0500

    Lab2 update

--- powershell/welcome.ps1
+++ powershell/welcome.ps1
@@ -1,10 +1,25 @@
 # Lab 2 COMP2101 welcome script for profile
 #
+$env:path += ";$HOME/documents/github/comp2101/powershell"
+#----------------------------------------------------------------------------------
+new-item -path alias:np -value notepad | out-null
 
-function welcome {
-write-output "Welcome to planet $env:computername Overlord $env:username"
-$now = get-date -format 'HH:MM tt on dddd'
-write-output "It is $now."
+function get-cpuinfo {
+	get-ciminstance cim_processor | Select-Object manufacturer,name,numberofcores,currentclockspeed,Maxclockspeed
 }
 
+function welcome {
+	write-output "Welcome to planet $env:computername Overlord $env:username"
+	$now = get-date -format 'HH:MM tt on dddd'
+	write-output "It is $now.`n"
+}
+
+function get-mydisks {
+    get-wmiobject win32_diskdrive | foreach {
+        new-object -typename psobject -property @{Manufacturer=$_.Manufacturer;Model=$_.Model;
+            SerialNumber=$_.SerialNumber;FirmwareRevision=$_.FirmwareVersion;Size=($_.size / 1GB).tostring() + " GB"}
+    }
+} 
+#----------------------------------------------------------------------------------
+get-cpuinfo | fl
 welcome
